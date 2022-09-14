68022f0f41f9a4e44f3114b999bd99b83b9a2fc8
Aldrin <cleetusaldrin30@gmail.com>
2022-09-14 14:13:24 +0530

    most of the design finished

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,12 +1,13 @@
+import RideType from "./RideType";
 import Stats from "./Stats";
 
 const Navigation = () => {
     return ( 
-        <div className="my-auto has-background-grey has-text-centered">
-            <div className="map-container">
-                <div className="map has-background-black rounded">
+        <div className="my-auto  has-text-centered">
+            <div className="map-container rounded">
+                {/* <div className="map has-background-black rounded">
                     <img src="images/test.png" alt="" />
-                </div>
+                </div> */}
 
                 {/* <div className="stats-container has-text-white p-5 rounded" id="duration">
                     <div className="count is-size-2">
@@ -19,6 +20,36 @@ const Navigation = () => {
 
                 <Stats name={"Distance"} id={"distance"} count={"25 km"}></Stats>
                 <Stats name={"Duration"} id={"duration"} count={"18 min"}></Stats>
+
+
+                {/* Turn these two into components if u can */}
+                <div className="destination-from " >
+                        <div class="field mt-5">
+                        <div class="control has-icons-left">
+                        <input class=" input placeholder-color-white has-background-black has-text-white is-rounded is-large " type="email" placeholder="From"/>
+                        <span class="icon is-medium is-left">
+                            <img src="images/button1.svg" alt=""/>
+                        </span>
+                    </div>
+                    </div>
+                </div>
+
+                <div className="destination-to">
+                    <div class="field mt-5">
+                            <div class="control has-icons-left">
+                            <input class="input is-rounded is-large placeholder-color-white has-background-black has-text-white" type="email" placeholder="To"/>
+                            <span class="icon is-medium is-left">
+                            <img src="images/button2.svg" alt="" />
+                            </span>
+                        </div>
+                        </div>
+                </div>
+
+
+                <RideType id={"economy"}></RideType>
+                <RideType id={"premium"}></RideType>
+
+            
 
 
             </div>
---
+++ src/components/RideType.js
@@ -0,0 +1,21 @@
+const RideType = ({id}) => {
+    return ( 
+        <a href="">
+            <div className="stats-container has-text-white p-5 rounded" id={id}>
+        
+        {/* Empty Character for consistent sizing */}
+        <div className="is-size-2">&#x200B;</div> 
+
+            <div className="cab-pic pop">
+                <img src={`images/${id}.png`} alt="test" />
+            </div>
+
+            <div className="status-name has-text-primary">
+                Economy
+            </div>
+        </div>
+        </a>
+     );
+}
+ 
+export default RideType;
